d4f1e95c8bdc217958b019a59c89b54b7c5fd267
Jon Rexhbogaj <jon.rexhbogaj@student.uni-pr.edu>
2021-10-03 21:42:00 +0200

    validation

--- BettAPI/Data/Bett.cs
+++ BettAPI/Data/Bett.cs
@@ -13,15 +13,14 @@ namespace BettAPI.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
+        [Required]
         public double Amount { get; set; }
+        [Required]
         public string Type { get; set; }
-
+        [Required]
         public string Odds { get; set; }
 
         public bool isUpdated { get; set; }
-        
-        //public DateTime CreatedTime { get; set; }
         public DateTime UpdatedTime { get; set; }
 
 
