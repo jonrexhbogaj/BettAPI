fe759a2e972029399d9b7f395c05cabb2bcdc815
Jon Rexhbogaj <jon.rexhbogaj@student.uni-pr.edu>
2021-10-04 22:33:32 +0200

    removing only // for authorize

--- BettAPI/Controllers/BettsController.cs
+++ BettAPI/Controllers/BettsController.cs
@@ -15,7 +15,7 @@ namespace BettAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
     public class BettsController : ControllerBase
     {
         private readonly IBettRepository _bettRepository;
